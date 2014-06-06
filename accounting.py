8628e3300ef4bf520c2a934dd18ea723b74ec281
katiecheng <katie.cheng@gmail.com>
2014-06-05 23:57:17 -0700

    Separated code into distinct functions

--- accounting.py
+++ accounting.py
@@ -3,40 +3,83 @@
 # Someone separated sales into online sales and phone sales.
 # Someone produced a fancy report to summarize the information for our CEO
 
+def melon_tally():
 
-def main():
-    print "******************************************"
+    """Count up the differerent types of melons that were sold."""
+
     f = open("orders_by_type.csv")
-    melon_tallies = {"Musk": 0, "Hybrid": 0, "Watermelon": 0, "Winter": 0}
+
+    melon_tallies = ({
+        "Musk": 0, 
+        "Hybrid": 0, 
+        "Watermelon": 0, 
+        "Winter": 0})
+
     for line in f:
         data = line.split(",")
         melon_type = data[1]
         melon_count = int(data[2])
         melon_tallies[melon_type] += melon_count
+
     f.close()
-    melon_prices = { "Musk": 1.15, "Hybrid": 1.30, "Watermelon": 1.75, "Winter": 4.00 }
+
+    return melon_tallies
+
+def calculate_revenue(melon_tallies):
+
+    """Calculate the revenue from melon tallies."""
+
+    melon_prices = ({ 
+        "Musk": 1.15, 
+        "Hybrid": 1.30, 
+        "Watermelon": 1.75, 
+        "Winter": 4.00 })
+
     total_revenue = 0
+
     for melon_type in melon_tallies:
         price = melon_prices[melon_type]
         revenue = price * melon_tallies[melon_type]
         total_revenue += revenue
-        print "We sold %d %s melons at %0.2f each for a total of %0.2f" % (melon_tallies[melon_type], melon_type, price, revenue)
-    print "******************************************"
+        print ("We sold %d %s melons at %0.2f each for a total of %0.2f" 
+            % (melon_tallies[melon_type], melon_type, price, revenue))
+
+def sales_type():
+
+    """Separate sales into online sales and phone sales."""
+
     f = open("orders_with_sales.csv")
+
     sales = [0, 0]
+
     for line in f:
         data = line.split(",")
         if data[1] == "0":
             sales[0] += float(data[3])
         else:
             sales[1] += float(data[3])
+
     print "Salespeople generated %0.2f in revenue." % sales[1]
     print "Internet sales generated %0.2f in revenue." % sales[0]
+
     if sales[1] > sales[0]:
         print "Guess there's some value to those salespeople after all."
     else:
         print "Time to fire the sales team! Online sales rule all!"
+
+def fancy_report():
+
+    """Produce a fancy report to summarize the information for our CEO"""
+
     print "******************************************"
+    calculate_revenue(melon_tally())
+    print "******************************************"
+    sales_type()
+    print "******************************************"
+
+
+def main():
+    fancy_report()
 
 
 if __name__ == "__main__":
